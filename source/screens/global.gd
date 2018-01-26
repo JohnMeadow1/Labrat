f18e41987a0913e724059557dcb3a7dbc6d6c911
JohnMeadow <leszek.nowak@uj.edu.pl>
2018-01-26 22:53:28 +0100

    mapa_chodzienie

--- source/screens/global.gd
+++ source/screens/global.gd
@@ -1,31 +1,20 @@
 extends Node
 
-const PLACEMENT_POINTS_PER_BALL = 10
-const BALL_DISTANCE_FOR_COMBO   = 30 # pixels
-const MIN_BALL_COUNT_FOR_COMBO  = 2
-const BALL_OFFSET               = Vector2(0,-130)
-const LEVELS_IN_MENU            = 3
-var current_level               = 0
+var map_pos                     = Vector2(5,5)
+var map_orientation             = 0
+var screen_res                  = Vector2()
+var update_res                  = true
 
-#var BASE_BALL_RADIUS            = 50
-#const TOOL_FILL_RADIUS          = 160
-#var   FILL_BALL_RADIUS          = 0.6 * BASE_BALL_RADIUS
-#
-#var BALL_COLOR_COUNT            = 3 
-#var color_array                 = []
-
-#var blocked_zone_color          = Color( 0.4, 0.4, 0.4 )
-
-#var viewport_res   = Vector2()
-var screen_res     = Vector2()
-var update_res     = false
+var map_packed = preload("res://screens/play/map/map.tscn")
+onready var map = map_packed.instance()
 
 func _ready():
 	screen_res   = OS.get_window_size()
-#	viewport_res = get_node("/root").get_children()[1].get_viewport_rect().size
-	set_fixed_process(true)
+	
+#	set_fixed_process(true)
 	
 func _fixed_process(delta):
+	
 	if (OS.get_window_size().x != screen_res.x):
 		screen_res = OS.get_window_size()
 		update_res = true
--- source/screens/play/main.gd
+++ source/screens/play/main.gd
@@ -17,9 +17,23 @@ func _input(ev):
 	
 func _fixed_process(delta):
 	if mouse_click_state:
-		get_node("mouse_clicked").set_pos(mouse_click_pos)
+#		get_node("mouse_clicked").set_pos(mouse_click_pos)
 		print ("Clicked at: ",mouse_click_pos)
+		if mouse_click_pos.x>500:
+			GLOBAL.map_orientation -= 1
+		if mouse_click_pos.x<100:
+			GLOBAL.map_orientation += 1
+		if mouse_click_pos.y>500:
+			GLOBAL.map_pos -= Vector2( cos(GLOBAL.map_orientation*(PI/2)),-sin(GLOBAL.map_orientation*(PI/2)))
+		if mouse_click_pos.y<100:
+			GLOBAL.map_pos += Vector2( cos(GLOBAL.map_orientation*(PI/2)),-sin(GLOBAL.map_orientation*(PI/2)))
+		GLOBAL.map_orientation %= 4
+#		print( GLOBAL.map_orientation )
+	get_node("map/selector").set_pos( GLOBAL.map_pos * 80 +Vector2(40,40) )
+	get_node("map/selector").set_rot( GLOBAL.map_orientation*(PI/2) )
 
 func _ready():
 	set_fixed_process(true)
 	set_process_input(true)
+	
+	
