32cf8eb3369d8c570c9ff398c1ef562fed0f785b
Tomasz Szymański <cl.arctos.orion@gmail.com>
2018-01-27 02:11:55 +0100

    functional keypad door.

--- source/screens/play/items/keypad.gd
+++ source/screens/play/items/keypad.gd
@@ -1,6 +1,7 @@
 extends "res://screens/play/items/abstract_item.gd"
 
-
+export (String) var valid_code
+var is_open = false
 
 func set_variables():
 	mini_scale    = 0.25
@@ -11,9 +12,18 @@ func process_item():
 	pass
 	
 func enable():
-	target_scale = 1
-	get_node("Area2D/Screen").set_hidden(false)
+	if(!is_open):
+		target_scale = 1
+		get_node("Area2D/Screen").set_hidden(false)
 	
 func dissable():
 	target_scale = 0.25
 	get_node("Area2D/Screen").set_hidden(true)
+
+func validateCode(code):
+	if (code == valid_code):
+		is_open = true
+		get_node("Light").set_modulate(Color(0,255,0))
+		dissable()
+		animate = true;
+	pass
